3f94e194c7e6c7920a95680f83f3a5507268981e
Shun <shun1048576@zoho.com>
2018-12-03 19:13:25 +0800

    Start factoring

--- akane-lang/letters.h
+++ akane-lang/letters.h
@@ -4,65 +4,91 @@ DEFINE_AKANE_EXCEPTION(Runtime)
 
 namespace AkaneLang
 {
-	// "字母" 是本编译器中一切符号 (自动机中的符号, 文法中的终结符/非终结符) 的统称.
+	/*
+	
+	Letter
+
+		是形式语言中所有符号的基类.
+		
+			重载了比较运算符;
+			getShortDescription, getLongDescription : 获取人可读的文本的接口;
+			getUniqueName: 获取字符串形式的区别于其他 Letter 的唯一名称;
+			duplicate/duplicate_freeNeeded : 返回一个指向本 Letter 对象副本的指针.
+	
+	*/
 	struct Letter
 	{
-		static const Letter &epsilon() = delete;
-		static const Letter &elseLetter() = delete;
-		virtual bool operator<(const Letter &r) const { return false; }
-		virtual bool operator>(const Letter &r) const { return false; }
-		virtual bool operator==(const Letter &r) const { return false; }
-		virtual bool operator<=(const Letter &r) const { return false; }
-		virtual bool operator>=(const Letter &r) const { return false; }
-		virtual bool operator!=(const Letter &r) const { return false; }
-		virtual std::string getLetterDescription() const = 0;
+		virtual bool operator<(const Letter &l) const { return getUniqueName() < l.getUniqueName(); }
+		virtual bool operator>(const Letter &l) const { return getUniqueName() > l.getUniqueName(); }
+		virtual bool operator<=(const Letter &l) const { return getUniqueName() <= l.getUniqueName(); }
+		virtual bool operator>=(const Letter &l) const { return getUniqueName() >= l.getUniqueName(); }
+		virtual bool operator==(const Letter &l) const { return getUniqueName() == l.getUniqueName(); }
+		virtual bool operator!=(const Letter &l) const { return getUniqueName() != l.getUniqueName(); }
+		virtual std::string getShortDescription() const = 0;
 		virtual std::string getLongDescription() const = 0;
+		virtual std::string getUniqueName() const = 0;
+		virtual Letter *duplicate_freeNeeded() const = 0;
+		virtual std::unique_ptr<Letter> duplicate() const = 0;
+
+		virtual ~Letter() = 0;
 	};
 
-	// "LetterString" : 
-	struct LetterString : public Letter
+	/*
+
+	StringifiedLetter
+
+		以内部存储的字符串形式来保留自身内容的 Letter 对象, 区别于 TokenizedLetter, 它是以内存.
+
+			重载了比较运算符;
+			getShortDescription, getLongDescription : 获取人可读的文本的接口;
+			getUniqueName: 获取字符串形式的区别于其他 Letter 的唯一名称;
+			duplicate/duplicate_freeNeeded : 返回一个指向本 Letter 对象副本的指针.
+
+	*/
+	struct StringifiedLetter : public virtual Letter
 	{
-		std::string text;
-		LetterString() : Letter()
+		std::string name;
+		StringifiedLetter() : Letter()
 		{
-			text = "(invalid)";
+			name = "(invalid)";
 		}
-		LetterString(const LetterString &_l) : Letter(_l)
+		StringifiedLetter(const StringifiedLetter &_l) : Letter(_l)
 		{
-			text = _l.text;
+			name = _l.name;
 		}
-		LetterString(const std::string &);
+		StringifiedLetter(const std::string &_name);
 
-		static const LetterString &epsilon();
-		static const LetterString &elseLetter();
-		virtual bool operator<(const Letter &r) const override;
-		virtual bool operator>(const Letter &r) const override;
-		virtual bool operator==(const Letter &r) const override;
-		virtual bool operator<=(const Letter &r) const override;
-		virtual bool operator>=(const Letter &r) const override;
-		virtual bool operator!=(const Letter &r) const override;
+		static const StringifiedLetter &epsilon();
+		static const StringifiedLetter &elseLetter();
 
-		virtual std::string getLetterDescription() const { return escape(text); }
-		virtual std::string getLongDescription() const { return escape(text); }
+		virtual std::string getUniqueName() const override { return name; }
+		virtual std::string getShortDescription() const { return escape(name); }
+		virtual std::string getLongDescription() const { return escape(name); }
+		virtual StringifiedLetter *duplicate_freeNeeded() const { return new StringifiedLetter(*this); }
+
+		virtual ~StringifiedLetter() {};
 	};
 
 	struct LetterGenerator
 	{
-		virtual const Letter &next_freeNeeded() = 0;
-		virtual const Letter &peek_freeNeeded() = 0;
+		virtual std::unique_ptr<Letter> next() = 0;
+		virtual std::unique_ptr<Letter> peek() = 0;
+		virtual const Letter *next_freeNeeded() = 0;
+		virtual const Letter *peek_freeNeeded() = 0;
+		virtual void ignoreOne() = 0;
 	};
 
-	struct LetterStringGenerator : public LetterGenerator
+	struct StreamedLetterGenerator : public LetterGenerator
 	{
-		// 务必保证在生存期内
-		std::istream &s;
+		std::istream *streamP;
 		std::streampos lastPos;
-		std::stringstream currWordStream;
-		virtual const LetterString &next_freeNeeded();
-		virtual const LetterString &peek_freeNeeded();
+		std::stringstream *currTokenStreamP;
+		virtual std::unique_ptr<NamedLetter> next();
+		virtual std::unique_ptr<NamedLetter> peek();
+		virtual const NamedLetter &next_freeNeeded();
+		virtual const NamedLetter &peek_freeNeeded();
 		template <typename T>
 		void dumpCurrWord(T &dest);
-		// 务必保证 _s 在生存期内
 		LetterStringGenerator(std::istream &_s);
 	};
 
